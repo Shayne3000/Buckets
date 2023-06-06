5a1961e57fc870c58806875425c5972db45e851d
Shayne3000 <alfred3000prime@hotmail.com>
2023-06-07 04:40:24 +0800

    setup db connection and data access for the items
    table.

---
+++ pkg/db/db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	HOST     = "postgres-db"
+	PORT     = 5432
+	PASSWORD = "buck"
+	DB_NAME  = "buckets_db"
+)
+
+type Database struct {
+	connection *sql.DB
+}
+
+var errorNoMatch = fmt.Errorf("The requested record does not exist in the table.")
+
+func InitializeDB(username, password, database string) (Database, error) {
+	db := Database{}
+
+	// Data source name for the postgres driver name
+	dbDSN := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", HOST, PORT, username, password, database)
+
+	conn, err := sql.Open("postgres", dbDSN)
+
+	if err != nil {
+		return db, err
+	}
+
+	db.connection = conn
+	err = db.connection.Ping()
+
+	if err != nil {
+		return db, err
+	}
+
+	log.Println("Database initialized and connection established.")
+
+	return db, nil
+}
---
+++ pkg/db/item.go
@@ -0,0 +1,10 @@
+package db
+
+// file responsible for interacting with the Items table in terms of CRUD ops with the item model
+import (
+	"github.com/Shayne3000/Buckets/pkg/models"
+)
+
+func (db Database) GetAllItems() (*models.ItemList, error) {
+
+}
