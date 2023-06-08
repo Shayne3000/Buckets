2520ba13d5be32abdf62764c601a97f37e63ddf0
Shayne3000 <alfred3000prime@hotmail.com>
2023-06-08 19:37:46 +0800

    added a function for creating an item/adding an
    item to the DB

--- pkg/db/item.go
+++ pkg/db/item.go
@@ -1,10 +1,41 @@
 package db
 
-// file responsible for interacting with the Items table in terms of CRUD ops with the item model
+// file responsible for interacting with the Items table in terms of CRUD ops with the item model.
 import (
 	"github.com/Shayne3000/Buckets/pkg/models"
 )
 
+func (db Database) AddItem(item *models.Item) error {
+	var id int
+
+	var created_at string
+
+	query := `INSERT INTO items (name, description) VALUES ($1 $2) RETURNING id, created_at`
+
+	err := db.connection.QueryRow(query, item.Name, item.Description).Scan(&id, &created_at)
+
+	if err != nil {
+		return err
+	}
+
+	item.ID = id
+	item.CreatedAt = created_at
+
+	return nil
+}
+
 func (db Database) GetAllItems() (*models.ItemList, error) {
+
+}
+
+func (db Database) GetItemById(id int) (*models.Item, error) {
+
+}
+
+func (db Database) UpdateItem(itemId int, itemData models.Item) (models.Item, error) {
+
+}
+
+func (db Database) DeleteItem(itemId int) error {
 
 }
