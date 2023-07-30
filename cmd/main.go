71b09adaaf595acf3837d1dcf103600d85b1b8dd
Shayne3000 <alfred3000prime@hotmail.com>
2023-07-29 22:53:46 -0400

    setup the webservice and connected its components
    in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/Shayne3000/Buckets/pkg/db"
+	"github.com/Shayne3000/Buckets/pkg/handler"
+)
 
 func main() {
-	fmt.Printf("hello")
+	serverPort := "8080"
+	dbUser := "postgres"
+
+	// setup the DB
+	database, err := db.InitializeDB(dbUser)
+
+	if err != nil {
+		log.Fatalf("Could not set up database: %v", err)
+	}
+	defer database.Connection.Close()
+
+	// setup the router and connect it to the database instance
+	router := handler.NewRouter(database)
+
+	// start the server
+	log.Fatal(http.ListenAndServe(serverPort, router))
 }
